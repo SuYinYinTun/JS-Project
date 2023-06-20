456a76b4304920973d7b5c06a43012bf9da45b67
SuYinYinTun <suyinyintun@gmail.com>
2023-06-20 02:00:34 -0700

    To Do List

--- ToDoList/dom.js
+++ ToDoList/dom.js
@@ -1,15 +1,45 @@
 var val;
 
-document.body.addEventListener("click",xyz);
+//add
+document.querySelector('form').addEventListener('submit',function(e){
+    e.preventDefault();
+    // console.log("hello");
 
-function xyz(e){
-    console.log("I am body");
+    var newtask;
+    // console.log(newtask);
+    var lists= document.getElementsByTagName("li");
 
-    if(e.target.className === "fas fa-trash-alt"){
-        console.log("I am trash");
+    if (newtask = document.getElementById("task").value){
+        lists[0].innerHTML=newtask + "<i class='fas fa-trash-alt rc1'>";
 
     }
 
-     e.target.parentElement.parentElement.remove(); 
 
-};
+
+});
+
+//delete
+
+document.body.addEventListener("click",xyz);
+
+function xyz(e){
+    //console.log("I am body");
+
+    if(e.target.className === "fas fa-trash-alt rc1"){
+        //console.log("I am trash");
+        e.target.parentElement.parentElement.remove(); 
+
+    }
+}
+
+document.body.addEventListener("click",clear);
+
+function clear(e){
+    //console.log("I am body");
+
+    if(e.target.className === "clear-tasks"){
+        //console.log("I am trash");
+        e.target.parentElement.remove(); 
+
+    }
+}
